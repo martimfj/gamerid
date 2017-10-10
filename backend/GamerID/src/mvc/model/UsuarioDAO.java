f690bfae44a8606cf4250ece974d93604bd316c8
Martim <martim.ferreira.jose@gmail.com>
2017-10-10 08:52:53 -0300

    Wip backend

--- backend/GamerID/src/mvc/model/UsuarioDAO.java
+++ backend/GamerID/src/mvc/model/UsuarioDAO.java
@@ -12,67 +12,69 @@ public class UsuarioDAO {
 		try {
 			 Class.forName("com.mysql.jdbc.Driver");
 			 connection = DriverManager.getConnection(
-			 "jdbc:mysql://localhost/meus_dados", "root", "123456");
+			 "jdbc:mysql://localhost/gamerid", "root", "123456");
 			 } 
 		catch (SQLException | ClassNotFoundException e) {e.printStackTrace();}}
 	
-	 public void adiciona(Usuario usuario) throws IOException {
+	 public void addUser(Usuario usuario) throws IOException {
 		 MultipartFile filePart = usuario.getAvatar();
 		 /* Rotina para salvar o arquivo no servidor */
 		 if (!filePart.isEmpty()) {
 			 String fileName = filePart.getOriginalFilename();
 			 File uploads = new File("/tmp");
-			 File file = new File(uploads, fileName);
-			 	try (InputStream input = filePart.getInputStream()) {
-			 		File.copy(input, file.toPath());
-			 	}
+				 File file = new File(uploads, fileName);
+				 	try (InputStream input = filePart.getInputStream()) {
+				 		File.copy(input, file.toPath());
+				 	}
 		 }
 		 
 		 try {
-			 String sql = "INSERT INTO usuario (login, senha, foto) values(?,?,?)";
+			 String sql = "INSERT INTO user (username, password, avatar) values(?,?,?)";
 			 PreparedStatement stmt = connection.prepareStatement(sql);
-			 stmt.setString(1,usuario.getLogin());
-			 stmt.setString(2,usuario.getSenha());
+			 stmt.setString(1,usuario.getUsername());
+			 stmt.setString(2,usuario.getPassword());
 			 stmt.setBinaryStream(3, filePart.getInputStream());
 			 stmt.execute();
 			 stmt.close();
 		 }
 		 catch (SQLException e) {e.printStackTrace();}}
 	 
-	 public boolean existeUsuario(Usuario usuario) {
-	 boolean existe = false;
-	 try {
-	 PreparedStatement stmt = connection.
-	 prepareStatement("SELECT COUNT(*) FROM usuario WHERE login=? AND senha=?
-	LIMIT 1");
-	 stmt.setString(1, usuario.getLogin());
-	 stmt.setString(2, usuario.getSenha());
-	 ResultSet rs = stmt.executeQuery();
-	 if(rs.next()){
-	 if(rs.getInt(1) != 0) {existe=true;}
-	 }
-	 rs.close();
-	 stmt.close();
-	 } catch(SQLException e) {System.out.println(e);}
-	 return existe;
-	 }
+	 public boolean existingUser(Usuario usuario) {
+		 boolean exists = false;
+		 try {
+			 PreparedStatement stmt = connection.
+			 prepareStatement("SELECT COUNT(*) FROM user WHERE username=? AND password=? LIMIT 1");
+			 stmt.setString(1, usuario.getUsername());
+			 stmt.setString(2, usuario.getPassword());
+			 ResultSet rs = stmt.executeQuery();
+			 if(rs.next()){
+				 if(rs.getInt(1) != 0) {exists = true;}
+			 }
+			 rs.close();
+			 stmt.close();
+		 	}
+		 	catch(SQLException e) {System.out.println(e);}
+		 return exists;
+		 }
 
-	 public byte[] buscaFoto(String login) {
-	 byte[] imgData = null;
-	 try {
-	 PreparedStatement stmt = connection.
-	 prepareStatement("SELECT * FROM usuario WHERE
-	login=? ");
-	 stmt.setString(1, login);
-	 ResultSet rs = stmt.executeQuery();
-	 if(rs.next()) {
-	 Blob image = rs.getBlob("foto");
-	 imgData = image.getBytes(1, (int) image.length());
-	 }
-	 rs.close();
-	 stmt.close();
-	 } catch(SQLException e) {System.out.println(e);}
-	 return imgData;
-	 }
+	 public byte[] searchAvatar(String username) {
+		 byte[] imgData = null;
+		 try {
+			 PreparedStatement stmt = connection.
+			 prepareStatement("SELECT * FROM user WHERE username=? ");
+			 stmt.setString(1, username);
+			 ResultSet rs = stmt.executeQuery();
+			 
+			 if(rs.next()) {
+				 Blob image = rs.getBlob("avatar");
+				 imgData = image.getBytes(1, (int) image.length());
+			 }
+			 
+			 rs.close();
+			 stmt.close();
+		 }
+		 catch(SQLException e) {System.out.println(e);}
+		 return imgData;
+		 }
 	}
-}
+
