3f4a9e6c5fc965e0875255b66cd802b285a686d9
filipefborba <filipefborba@gmail.com>
2017-10-14 19:30:40 -0300

    peguei infos do csgo

--- my-app/src/Steam.jsx
+++ my-app/src/Steam.jsx
@@ -12,10 +12,14 @@ class Steam extends Component {
                 personName: null,
                 steamId: null,
                 profileUrl: null,
-                avatarFullUrl: false,
-                countryCode: null,
-                stateId: null,
-                cityId: null
+                avatarFullUrl: null,
+                csgo: {
+                    kills: null,
+                    deaths: null,
+                    mvps: null,
+                    timePlayed: null,
+                    headshots: null,
+                }
             }
         };
         
@@ -41,6 +45,11 @@ class Steam extends Component {
     
 
     render() {
+
+        let csgoImage = 'http://media.steampowered.com/steamcommunity/public/images/apps/730/d0595ff02f5c79fd19b06f4d6165c3fda2372820.jpg';
+        let kdratio = this.state.steamprofile.csgo.kills / this.state.steamprofile.csgo.deaths;
+        let headshotPercentage = (this.state.steamprofile.csgo.headshots / this.state.steamprofile.csgo.kills) * 100
+
         return (
             <div style={ this.props.margin ? { marginLeft:275,marginRight:25 } : { marginLeft : 0 }}>
             <div className='row centered'>
@@ -49,7 +58,13 @@ class Steam extends Component {
                         <img src={this.state.steamprofile.avatarFullUrl} />
                         <p>{this.state.steamprofile.personName}</p>
                         <p>{this.state.steamprofile.profileUrl}</p>
-                        <p>{this.state.steamprofile.steamId}</p>
+                        <img src={csgoImage} />
+                        <p>Time Played: {this.state.steamprofile.csgo.timePlayed}</p>
+                        <p>Kills: {this.state.steamprofile.csgo.kills}</p>
+                        <p>Deaths: {this.state.steamprofile.csgo.deaths}</p>
+                        <p>MVPs: {this.state.steamprofile.csgo.mvps}</p>
+                        <p>K/D Ratio: {kdratio.toFixed(2)}</p>
+                        <p>Headshot %: {headshotPercentage.toFixed(2)}</p>
                     </div>
 
                     <div className='row center canvas-container'>
